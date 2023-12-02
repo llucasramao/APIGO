25510462d3a95970733b51263089a8d696898024
Lucas Ramão <ramaolucas01@gmail.com>
2023-12-02 12:13:19 -0300

    adiçÃO DE LOGS E SCHEMA

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/llucasramao/APIGO/router"
+import (
+	"github.com/llucasramao/APIGO/config"
+	"github.com/llucasramao/APIGO/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errf("Erro na conexão: %v", err)
+		return
+	}
+
 	router.Initialize()
 }

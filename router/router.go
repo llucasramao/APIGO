f199329bcf59b89fba2a6c92550d53b0b39b0627
Lucas Ramão <ramaolucas01@gmail.com>
2023-12-03 09:49:05 -0300

    feat: custom logs option

--- router/router.go
+++ router/router.go
@@ -5,6 +5,22 @@ import "github.com/gin-gonic/gin"
 func Initialize() {
 	// Create router using gin framework
 	router := gin.Default()
+	// router := gin.New()
+	// router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+	// 	// your custom format
+	// 	return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+	// 		param.ClientIP,
+	// 		param.TimeStamp.Format(time.RFC1123),
+	// 		param.Method,
+	// 		param.Path,
+	// 		param.Request.Proto,
+	// 		param.StatusCode,
+	// 		param.Latency,
+	// 		param.Request.UserAgent(),
+	// 		param.ErrorMessage,
+	// 	)
+	// }))
+	// router.Use(gin.Recovery())
 
 	// Initialize routes
 	InitializeRoutes(router)
